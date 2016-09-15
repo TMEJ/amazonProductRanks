1579fcb7df39f91d1b382faf9e749976f566e226
Kelly Douglass <kdouglass@gmail.com>
2016-09-15 14:57:16 -0500

    Added headers

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var request = require('request')
 var _ = require('lodash')
 
 
+var keywordsInput = process.argv[2]
+
+//for (var k = 0; k <= keywordsList.length - 1; k++) {
+
+
 for (var i = 1; i <=10; i++){
 
 var metadermASINS = ['B00BCPTH6U', 'B00F2LJVBQ', 'B00B9JTITK', 'B00F2LTMTW', 'B00FQ0RQR4', 'B00BCNIIVM']
+
+
 
 _.map(metadermASINS, test)
 
@@ -13,12 +20,25 @@ _.map(metadermASINS, test)
 function test(ASINS) {
 
   var page = i
-  var keywords = 'psoriasis'
+  var keywords = keywordsInput
 
   var url = 'https://www.amazon.com/mn/search/'
 
   var options = {
     url: url,
+    headers: {
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Encoding':'gzip, deflate, sdch, br',
+      'Accept-Language':'en-US,en;q=0.8',
+      'Avail-Dictionary':'DgmIdlSw',
+      'Cache-Control':'no-cache',
+      'Connection':'keep-alive',
+      'Host':'www.amazon.com',
+      'Pragma':'no-cache',
+      'Referer':'https://www.amazon.com/',
+      'Upgrade-Insecure-Requests':'1',
+      'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    },
     qs: {
       rh: `aps, k:${keywords}`,
       page: page,
@@ -59,5 +79,8 @@ function test(ASINS) {
 
   }
 
+};
 }
-}
+
+
+
